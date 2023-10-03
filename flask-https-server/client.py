3e1595ee6e86fcc800532920c384bc5ec5b812e5
rkruser <rkrusinga@gmail.com>
2023-10-03 17:31:06 -0400

    Update client.py

--- flask-https-server/client.py
+++ flask-https-server/client.py
@@ -6,8 +6,8 @@ url = "https://ryenandvivekstartup.online/login"
 #requests.packages.urllib3.disable_warnings()
 
 data = {
-    "username": "user2",
-    "password": "password2234"
+    "username": "user1",
+    "password": "password1"
 }
 
 response = requests.post(url, json=data)
