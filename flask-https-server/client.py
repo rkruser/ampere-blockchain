54d1bd6edd8c05826c341866c7d6877ef619b4b4
Ryen Krusinga <rkrusinga@gmail.com>
2023-10-03 19:08:13 -0400

    Added session keys

--- flask-https-server/client.py
+++ flask-https-server/client.py
@@ -1,6 +1,6 @@
 import requests
 
-url = "https://ryenandvivekstartup.online/login"
+login_url = "https://ryenandvivekstartup.online/login"
 
 # Disable warnings for self-signed certificates. Do NOT do this in production.
 #requests.packages.urllib3.disable_warnings()
@@ -10,5 +10,15 @@ data = {
     "password": "password1"
 }
 
-response = requests.post(url, json=data)
-print(response.json()['message'])
+response = requests.post(login_url, json=data)
+print(response.json())
+
+my_session_key = None
+if "session_key" in response.json():
+    my_session_key = response.json()["session_key"]
+    home_url = "https://ryenandvivekstartup.online/home"
+    data = {
+        "session_key": my_session_key
+    }
+    response = requests.post(home_url, json=data)
+    print(response.json())
--- flask-https-server/https_server.py
+++ flask-https-server/https_server.py
@@ -4,6 +4,7 @@ from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 from cryptography.hazmat.backends import default_backend
 #from base64 import urlsafe_b64encode, urlsafe_b64decode
 import os
+import time
 
 app = Flask(__name__)
 
@@ -26,9 +27,15 @@ def generate_salt(length=32):
 salt1 = generate_salt()
 salt2 = generate_salt()
 users = {
-    "user1": (salt1, hash_password("password1", salt1).hex()),
-    "user2": (salt2, hash_password("password2", salt2).hex())
+    "user1": (salt1.hex(), hash_password("password1", salt1).hex()),
+    "user2": (salt2.hex(), hash_password("password2", salt2).hex())
 }
+
+session_keys = {}
+
+api_keys = {}
+
+
 
 @app.route('/')
 def index():
@@ -42,7 +49,7 @@ def login():
     stored_password_entry = users.get(username, None)
 
     if stored_password_entry is not None:
-        salt = stored_password_entry[0]
+        salt = bytes.fromhex(stored_password_entry[0])
         stored_password_hash = stored_password_entry[1]
         try:
             # Compare the given password with the stored hashed password
@@ -54,14 +61,44 @@ def login():
                 backend=default_backend()
             )
             kdf.verify(password.encode(), bytes.fromhex(stored_password_hash))
-            # If verification is successful, print the IP and return success message
+
+            # Generate a session key
             client_ip = request.remote_addr
+            session_key = generate_salt().hex()
+            # Store the session key in the server's database along with client id and timestamp plus six hours
+            session_keys[session_key] = (username, client_ip, time.time() + 21600)
+            
             print(f"Client IP: {client_ip}")
-            return jsonify({"message": "Login successful!"}), 200
+            return jsonify({"message": "Login successful!", "session_key":session_key}), 200
         except:
             pass
 
     return jsonify({"message": "Invalid credentials!"}), 401
 
+
+@app.route('/home', methods=['POST'])
+def home():
+    session_key = request.json.get('session_key')
+    client_ip = request.remote_addr
+    if session_key in session_keys:
+        if session_keys[session_key][1] == client_ip:
+            # Check if session key has expired
+            if session_keys[session_key][2] > time.time():
+                # Return the data requested by the client
+                username = session_keys[session_key][0]
+                return jsonify({"message": f"API access for {username} successful!"}), 200
+            else:
+                # Session key has expired
+                del session_keys[session_key]
+                return jsonify({"message": "Session key has expired!"}), 401
+        else:
+            return jsonify({"message": "Client IP does not match original session!"}), 401
+    else:
+        # Session key not found
+        return jsonify({"message": "Session key not found!"}), 401
+
+
+
 if __name__ == '__main__':
+    # These paths should really be in a .env file that is configured to the host computer
     app.run(ssl_context=('C:/Certbot/live/ryenandvivekstartup.online/fullchain.pem', 'C:/Certbot/live/ryenandvivekstartup.online/privkey.pem'), host='0.0.0.0', debug=False, port=3000)
