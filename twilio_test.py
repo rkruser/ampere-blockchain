d1f96d6c7f086342b485228a8c7769bba8fe69f1
Ryen Krusinga <rkrusinga@gmail.com>
2023-10-11 13:21:38 -0400

    Added twilio and sendgrid test code

---
+++ twilio_test.py
@@ -0,0 +1,19 @@
+# Download the helper library from https://www.twilio.com/docs/python/install
+import os
+from twilio.rest import Client
+
+
+# Find your Account SID and Auth Token at twilio.com/console
+# and set the environment variables. See http://twil.io/secure
+account_sid = os.environ['TWILIO_SID']
+auth_token = os.environ['TWILIO_AUTH_TOKEN']
+client = Client(account_sid, auth_token)
+
+message = client.messages \
+                .create(
+                     body="Hello, this is a test message.",
+                     #from_='+15017122661',
+                     to='+12692231472'
+                 )
+
+print(message.sid)
