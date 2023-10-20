620c7bf6505466fa3564aa3ed482fbb3b02e337d
rkruser <rkrusinga@gmail.com>
2023-10-20 15:43:46 -0400

    Added identity

--- full-app-mockup/core/network/identity.py
+++ full-app-mockup/core/network/identity.py
@@ -10,3 +10,28 @@ Purpose:
 - Generate/access local secret keys (or access remote-stored secret keys)
 
 """
+
+import json
+
+
+class Identity:
+    def __init__(self, config_file):
+        with open(config_file, 'r') as f:
+            self.config = json.load(f)
+        self.port = self.config["docker_port"]
+        self.hostname = self.config["hostname"]
+        self.lan_ip_address = self.config.get("lan_ip_address",None)
+        self.wan_ip_address = self.config.get("wan_ip_address", None)
+        self.role = self.config["role"]
+        self.private_key = self.config["private_key"]
+        self.public_key = self.config["public_key"]
+
+        self.database_host = self.config["database_host"]
+        self.database_port = int(self.config["database_port"])
+        self.database_name = self.config["database_name"]
+
+        self.zmq_port = self.config["zmq_port"]
+        self.zmq_host = self.config["zmq_host"]
+
+
+
