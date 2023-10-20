578438dfd6b9dc7a92fa10bbfbb58cf091f1ebbd
Ryen Krusinga <rkrusinga@gmail.com>
2023-10-20 11:41:10 -0400

    Added initial database code

--- full-app-mockup/core/database/mongo_database.py
+++ full-app-mockup/core/database/mongo_database.py
@@ -1,5 +1,8 @@
 from .database import Database
+from pymongo import MongoClient
 
 class MongoDatabase(Database):
-    def __init__(self, database_info, database_options=None):
-        pass
+    def __init__(self, database_info):
+        super().__init__(database_info)
+        self.client = MongoClient(self.database_info['host'], self.database_info['port'])
+        self.db = self.client[self.database_info['database_name']]
---
+++ full-app-mockup/core/database/user_mongo_database.py
@@ -0,0 +1,71 @@
+import datetime
+import random
+import string
+from .user_database_interface import UserDatabaseInterface
+from .mongo_database import MongoDatabase
+
+class UserMongoDatabase(UserDatabaseInterface, MongoDatabase):
+    def __init__(self, database_info):
+        MongoDatabase.__init__(self, database_info)
+        self.users = self.db['users']
+        self.sessions = self.db['sessions']
+
+    def _generate_random_string(self, length=32):
+        return ''.join(random.choice(string.ascii_letters + string.digits) for _ in range(length))
+
+    def login_user(self, username, password):
+        user = self.users.find_one({"username": username, "password": password})  # Consider hashing the password for better security
+        if not user:
+            return False  # or raise an appropriate error
+
+        session_id = self._generate_random_string()
+        expiration_time = datetime.datetime.now() + datetime.timedelta(hours=24)
+        
+        with self.client.start_session() as session:
+            with session.start_transaction():
+                self.sessions.insert_one({
+                    "session_id": session_id,
+                    "username": username,
+                    "expiration_time": expiration_time
+                }, session=session)
+
+                self.users.update_one(
+                    {"username": username},
+                    {"$push": {"active_sessions": session_id}},
+                    session=session
+                )
+
+        return True, session_id
+
+    def logout_user(self, username, session_id):
+        with self.client.start_session() as session:
+            with session.start_transaction():
+                self.sessions.delete_one({"session_id": session_id}, session=session)
+                self.users.update_one(
+                    {"username": username},
+                    {"$pull": {"active_sessions": session_id}},
+                    session=session
+                )
+
+    def verify_user_session(self, session_username, session_id):
+        session = self.sessions.find_one({"session_id": session_id, "username": session_username})
+        if not session:
+            return False
+        return datetime.datetime.now() < session['expiration_time']
+
+    def add_user(self, username, password, email, phone_number, permission_level):
+        # Consider hashing the password for better security
+        self.users.insert_one({
+            "username": username,
+            "password": password,
+            "email": email,
+            "phone_number": phone_number,
+            "permission_level": permission_level,
+            "active_sessions": []
+        })
+
+    def has_admin_permissions(self, username):
+        user = self.users.find_one({"username": username})
+        if not user:
+            return False  # or raise an appropriate error
+        return user['permission_level'] == 'admin'
